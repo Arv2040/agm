b6d45d9ca9f4e79a0e0b2ecb3df09bbc86c551ff
Arv2040 <aravcr4085@gmail.com>
2025-08-28 19:49:54 +0530

    feat: added llm

---
+++ backend/llm/llm.py
@@ -0,0 +1,33 @@
+from groq import Groq
+from dotenv import load_dotenv
+load_dotenv()
+def llminfer(prompt: str, model: str = "llama-3.3-70b-versatile", temperature: float = 1, max_tokens: int = 1024):
+    
+    client = Groq()
+    full_output = ""
+
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        temperature=temperature,
+        max_completion_tokens=max_tokens,
+        top_p=1,
+        stream=True,
+        stop=None
+    )
+
+    for chunk in completion:
+        delta_content = chunk.choices[0].delta.content
+        if delta_content:
+            print(delta_content, end="", flush=True)
+            full_output += delta_content
+
+    print("\n--- Completion finished ---")
+    return full_output
+
+
+# Example usage
+if __name__ == "__main__":
+
+    text = llminfer("Write a short story about a cat and a robot.")
+    print(text)
